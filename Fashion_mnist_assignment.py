2dd9ec48473bb20cc87e80b2ae9842d07ce478ca
larsbarstad <lmlovold@stud.ntnu.no>
2025-11-20 12:39:26 +0100

    imports

---
+++ Fashion_mnist_assignment.py
@@ -0,0 +1,9 @@
+#imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
